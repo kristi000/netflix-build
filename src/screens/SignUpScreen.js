f82b01918d5350021b92dcc48145d5e05ba131a7
kristish000 <kristish000@gmail.com>
2023-05-16 22:14:29 +0200

    login screen css update

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,31 +1,20 @@
 import React, { useState } from "react"
 import "./SignUpScreen.css"
 
-// gg
 function SignUpScreen() {
   const [signIn, setSignIn] = useState(false)
   return (
-    <div>
+    <div className="signupScreen">
       <h1>Sign Up</h1>
-      <div className="signupScreen__input">
+      <div className="signupScreenInput">
         <form>
           <input type="email" placeholder="Email Address" />
-          <button
-            onClick={() => setSignIn(true)}
-            className="signupScreen__getStarted"
-          >
-            GET STARTED
-          </button>
-
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign Up</button>
           <h4>
-            <span className="signupScreen__gray">
-              Already have an account?{" "}
-            </span>
-            <span
-              className="signupScreen__link"
-              onClick={() => setSignIn(true)}
-            >
-              Sign In.
+            <span className="signupScreenGray"> New to Netflix? </span>
+            <span className="signupScreenLink" onClick={() => setSignIn(true)}>
+              Sign In now.
             </span>
           </h4>
         </form>
