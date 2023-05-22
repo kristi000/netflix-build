4eb0173459950eec237f6ba4d84a8f69d16a5c8d
kristish000 <kristish000@gmail.com>
2023-05-22 15:12:59 +0200

    prf screen updates

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
 import "./Nav.css"
+import { useNavigate } from "react-router-dom"
 
 function Nav() {
   const [show, handleShow] = useState(false)
+  const navigate = useNavigate()
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -20,11 +22,13 @@ function Nav() {
     <div className={`navBar ${show && "navBlack"}`}>
       <div className="navContents">
         <img
+          onClick={() => navigate("/")}
           className="navLogo"
           src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
           alt="Netflix Logo"
         />
         <img
+          onClick={() => navigate("/profile")}
           className="navAvatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt="Netflix Avatar"
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,37 @@
 import React from "react"
 import "./ProfileScreen.css"
+import Nav from "../Nav"
+import { useSelector } from "react-redux"
+import { selectUser } from "../features/userSlice"
+import { auth } from "../firebase"
 
 function ProfileScreen() {
+  const user = useSelector(selectUser)
+
   return (
     <div className="profileScreen">
-      <h1>This is the Profile Screen</h1>
+      <Nav />
+      <div className="profileScreenBody">
+        <h1>Edit Profile</h1>
+        <div className="profileScreenInfo">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="Netflix Avatar"
+          />
+          <div className="profileScreenDetails">
+            <h2>{user.email}</h2>
+            <div className="profileScreenPlans">
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreenSignOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
